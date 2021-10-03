536cdbd4e1183ff04a72dd9e830bc817e1d3b93d
shanecolby <shanecolbyzero8@gmail.com>
2021-10-03 11:45:07 -0500

    enable pacman movement thoughout the entire grid

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ let pacmanCurrentIndex = 490
 squares[pacmanCurrentIndex].classList.add("pacman")
 
 function control(e) {
+  squares[pacmanCurrentIndex].classList.remove("pacman")
   // if (e.keyCode === 40) {
   //   console.log('pressed down')
   // } else if (e.keyCode === 38) {
@@ -79,17 +80,25 @@ function control(e) {
   switch (e.keyCode) {
     case 40:
       console.log('pressed down')
+      if (pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width
       break
+
     case 38:
       console.log('pressed up')
+      if (pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width
       break
+
     case 37:
       console.log('pressed left')
+      if (pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -= 1
       break
+
     case 39:
       console.log('pressed right')
+      if (pacmanCurrentIndex % width < width - 1) pacmanCurrentIndex += 1
       break
 
   }
+  squares[pacmanCurrentIndex].classList.add('pacman')
 }
 document.addEventListener("keyup", control)
