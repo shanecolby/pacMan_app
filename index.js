5e749f0e71b4431ff084539832e684e66c6aa2e8
shanecolby <shanecolbyzero8@gmail.com>
2021-10-06 16:26:22 -0500

    pacman eating pacdots and displaying score with innerHtml

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const width = 28
 const grid = document.querySelector(".grid")
 const scoreDisplay = document.getElementById("score")
 let squares = []
+let score = 0
 
 
 // 0 - pac-dots
@@ -119,5 +120,16 @@ function control(e) {
 
   }
   squares[pacmanCurrentIndex].classList.add('pacman')
+  pacDotEaten()
 }
 document.addEventListener("keyup", control)
+
+function pacDotEaten() {
+  if (squares[pacmanCurrentIndex].classList.contains("pac-dot")) {
+    squares[pacmanCurrentIndex].classList.remove("pac-dot")
+    score++
+    scoreDisplay.innerHTML = score
+  }
+
+}
+
